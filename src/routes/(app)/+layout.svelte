57b17e7ab32c0c38cdb7aecad123e36606b8e1c9
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-06-30 08:35:10 +0100

    added clerk

--- src/routes/(app)/+layout.svelte
+++ src/routes/(app)/+layout.svelte
@@ -8,13 +8,16 @@
 	<title>{$page.data.title}</title>
 	{#if $page.data.meta_description}
 		<meta name="description" content={$page.data.meta_description} />
+		<meta property="twitter:description" content={$page.data.meta_description} />
 	{/if}
 	{#if $page.data.meta_title}
 		<meta name="og:title" content={$page.data.meta_title} />
+		<meta property="twitter:title" content={$page.data.meta_title} />
 	{/if}
 	{#if $page.data.meta_image}
 		<meta name="og:image" content={$page.data.meta_image} />
 		<meta name="twitter:card" content="summary_large_image" />
+		<meta property="twitter:image" content={$page.data.meta_image} />
 	{/if}
 </svelte:head>
 <main>
---
+++ src/routes/(auth)/+layout.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { ModeWatcher } from 'mode-watcher';
+</script>
+
+<svelte:head>
+	<title>Sign In</title>
+	<meta name="description" content="Sign in to Ceetish" />
+	<meta name="og:title" content="Sign in" />
+	<meta
+		name="og:image"
+		content="https://images.prismic.io/ekidz/ZmwsMZm069VX1wUE_OgImage_2200x1237.webp?auto=format%2Ccompress&rect=0%2C41%2C2200%2C1155&w=2400&h=1260"
+	/>
+	<meta name="twitter:card" content="summary_large_image" />
+</svelte:head>
+
+<ModeWatcher />
+<main class="flex h-[100vh] w-full items-center justify-center">
+	<slot />
+</main>
