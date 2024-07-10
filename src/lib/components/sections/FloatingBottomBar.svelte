82945541b79e8664d26f715100867efc404f57d7
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-10 09:28:59 +0100

    Update FloatingBottomBar.svelte

--- src/lib/components/sections/FloatingBottomBar.svelte
+++ src/lib/components/sections/FloatingBottomBar.svelte
@@ -4,13 +4,29 @@
 	import SearchDialog from '../widgets/SearchDialog.svelte';
 	import { page } from '$app/stores';
 	import FilterDrawer from '../widgets/FilterDrawer.svelte';
+	import { onMount } from 'svelte';
+	import Skeleton from '../ui/skeleton/skeleton.svelte';
 
 	$: allFilters = $page.data.allFilters
 	$: actualFilters = $page.data.actualFilters
 	const showFilter = $page.url.pathname.toLowerCase() === "/catalog"
+
+	let mounted = new Promise((resolve, reject) => {
+		onMount(() => {
+			resolve(true);
+		});
+	});
 </script>
 
 <div class="sticky bottom-0 left-0 w-full md:hidden  bg-white dark:bg-secondary z-[2]">
+	{#await mounted}
+	<div class="h-14 flex justify-between items-center center">
+		<Skeleton class="w-10 h-10" />
+		<Skeleton class="w-10 h-10" />
+		<Skeleton class="w-10 h-10" />
+		<Skeleton class="w-10 h-10" />
+  </div>
+	{:then value}
 	<div class="h-14 flex justify-between items-center center">
 		<SearchDialog class="block md:hidden" />
 		<ModeToggle />
@@ -19,4 +35,5 @@
 		{/if}
 		<NavLinkDrawer />
   </div>
+	{/await}
 </div>
