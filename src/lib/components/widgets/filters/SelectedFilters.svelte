da20a39f0be4502f4da49c4eadefa07f76ddf755
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-11 15:01:39 +0100

    Update SelectedFilters.svelte

--- src/lib/components/widgets/filters/SelectedFilters.svelte
+++ src/lib/components/widgets/filters/SelectedFilters.svelte
@@ -34,7 +34,7 @@
 
 <div class={cn("grid grid-flow-col auto-cols-max overflow-auto gap-2 py-2 w-full md:w-fit", className)}>
   {#each params as item (item.key)}
-  <Button on:click={() => removeParam(item.key)} variant="amazonchipfilter" class="flex items-center gap-2 bg-white dark:bg-primary/5">
+  <Button on:click={() => removeParam(item.key)} variant="amazonchipfilter" class="flex items-center gap-2 bg-white dark:bg-secondary/5">
     <span>{item.value}</span>
     <X class="w-4 h-4" />
   </Button>
