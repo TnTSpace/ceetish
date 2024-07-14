60d0807098138ef0738afda31380f0b6ca8ec9b8
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-14 15:52:22 +0100

    Update SK_Header.svelte

--- src/lib/components/skeletons/SK_Header.svelte
+++ src/lib/components/skeletons/SK_Header.svelte
@@ -1,27 +1,26 @@
 <script lang="ts">
 	import Skeleton from '../ui/skeleton/skeleton.svelte';
-
 </script>
 
 <header class="sticky left-0 top-0 z-[3] bg-white dark:bg-secondary">
 	<nav
 		aria-label="Main"
-		class="mx-auto flex items-center justify-between px-2 h-14 font-medium center"
+		class="mx-auto flex h-14 items-center justify-between px-2 font-medium center"
 	>
 		<div class="flex items-center gap-4">
-      <Skeleton class="w-[108px] h-10" />
+			<Skeleton class="h-10 w-[108px]" />
 
-      <Skeleton class="w-[225px] h-9 hidden md:flex" />
+			<Skeleton class="hidden h-9 w-[225px] md:flex" />
 		</div>
 
 		<div class="flex items-center gap-2">
-      <Skeleton class="w-[57px] h-5 hidden md:flex" />
-      <Skeleton class="w-[57px] h-5 hidden md:flex" />
-      <Skeleton class="w-[57px] h-5 hidden md:flex" />
-      <Skeleton class="w-[57px] h-5 hidden md:flex" />
-      <Skeleton class="w-9 h-9" />
-      <Skeleton class="w-9 h-9" />
-      <Skeleton class="w-9 h-9 hidden md:flex" />
+			<Skeleton class="hidden h-5 w-[57px] md:flex" />
+			<Skeleton class="hidden h-5 w-[57px] md:flex" />
+			<Skeleton class="hidden h-5 w-[57px] md:flex" />
+			<Skeleton class="hidden h-5 w-[57px] md:flex" />
+			<Skeleton class="h-9 w-9" />
+			<Skeleton class="h-9 w-9" />
+			<Skeleton class="hidden h-9 w-9 md:flex" />
 		</div>
 	</nav>
 </header>
