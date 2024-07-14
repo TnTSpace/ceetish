4ea90a06d22296c297eac07db821f461220323b0
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-14 10:35:44 +0100

    shipping address to all countries

--- src/lib/slices/Bento/index.svelte
+++ src/lib/slices/Bento/index.svelte
@@ -9,7 +9,7 @@
 <section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
 	<div class="center grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 bg-primary/10">
 		{#each slice.primary.categories as category, i}
-			<div class="relative bg-white dark:bg-primary/10 rounded-lg overflow-hidden w-full aspect-[16/6] sm:aspect-square">
+			<div class="relative bg-white shadow-custom rounded-lg overflow-hidden w-full aspect-[16/6] sm:aspect-square">
 				<PrismicLink field={category.link} class="absolute top-0 left-0 w-full h-full z-[2]" />
 				<h2 class="p-4 bg-black/70 text-white absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-[1] font-semibold w-fit whitespace-nowrap">
 					<PrismicText field={category.name} />
