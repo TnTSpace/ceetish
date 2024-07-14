535f6ea0b463f40b29a39029777c191b4b0517c0
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-14 12:46:28 +0100

    Update ContactForm.svelte

--- src/lib/components/widgets/ContactForm.svelte
+++ src/lib/components/widgets/ContactForm.svelte
@@ -5,6 +5,7 @@
 	import Textarea from '../ui/textarea/textarea.svelte';
 	import type { iStatus } from '$lib/interfaces';
 	import SpinLoader from '../icons/SpinLoader.svelte';
+	import { Card } from '../ui/card';
 
 	$: loading = false;
 
@@ -40,19 +41,22 @@
 	};
 </script>
 
-<form on:submit={onSubmit} class="flex flex-col gap-4">
-	<div class="grid grid-cols-1 md:grid-cols-2 gap-4">
-		<Input type="text" name="firstName" placeholder="First name" />
-		<Input type="text" name="lastName" placeholder="Last name" />
-		<Input type="email" name="email" placeholder="Email" />
-		<Input type="text" name="phoneNumber" placeholder="Phone number" />
-	</div>
-	<Textarea name="message" class="resize-none" placeholder="Message" rows={8} />
-	{#if loading}
-		<Button type="button" disabled size="icon">
-			<SpinLoader />
-		</Button>
-	{:else}
-		<Button type="submit">Submit</Button>
-	{/if}
-</form>
+<Card class="bg-white dark:bg-inherit p-4">
+  <form on:submit={onSubmit} class="flex flex-col gap-4">
+    <h2>Alternatively, you can send us a message using the form below. We'll get back to you as soon as possible!</h2>
+    <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+      <Input type="text" name="firstName" placeholder="First name" />
+      <Input type="text" name="lastName" placeholder="Last name" />
+      <Input type="email" name="email" placeholder="Email" />
+      <Input type="text" name="phoneNumber" placeholder="Phone number" />
+    </div>
+    <Textarea name="message" class="resize-none" placeholder="Message" rows={8} />
+    {#if loading}
+      <Button type="button" disabled size="icon">
+        <SpinLoader />
+      </Button>
+    {:else}
+      <Button type="submit">Submit</Button>
+    {/if}
+  </form>
+</Card>
