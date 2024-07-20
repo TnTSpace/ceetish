ecc2a4a2557ca34d20dbe1a410080288260b13ac
DevDeducation <147517107+DevDeducation@users.noreply.github.com>
2024-07-20 01:39:03 +0100

    ui fixes

--- src/lib/components/ui/button/index.ts
+++ src/lib/components/ui/button/index.ts
@@ -6,7 +6,7 @@ const buttonVariants = tv({
 	base: "inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
 	variants: {
 		variant: {
-			default: "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+			default: "bg-primary dark:bg-white text-primary-foreground shadow hover:bg-primary/90",
 			destructive:
 				"bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
 			outline:
@@ -14,7 +14,7 @@ const buttonVariants = tv({
 			secondary: "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
 			ghost: "hover:bg-accent hover:text-accent-foreground",
 			link: "text-primary underline-offset-4 hover:underline",
-			amazonchip: "amazonchip",
+			amazonchip: "amazonchip dark:!border-none",
 			amazonchipfilter: "amazonchipfilter"
 		},
 		size: {
